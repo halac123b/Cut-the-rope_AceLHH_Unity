82b4424dfd7d458485f564586a8f28a1155f93ba
ntngan <ntngan020712@gmail.com>
2025-04-13 15:08:22 +0700

    CTR-23 Subtask: Add created object into parent “LevelObject”

--- Assets/Code/Scripts/DHa/LevelSceneLoader.cs
+++ Assets/Code/Scripts/DHa/LevelSceneLoader.cs
@@ -10,6 +10,8 @@ public class LevelSceneLoader : MonoBehaviour
     [SerializeField] private GameObject _ropePrefab;
     [SerializeField] private GameObject _frogPrefab;
     [SerializeField] private GameObject _starPrefab;
+
+    public Transform ParentCreatedObjects;
     private List<GameObject> _listLoadedObj = new();
 
     private void Start()
@@ -34,9 +36,11 @@ public class LevelSceneLoader : MonoBehaviour
         {
             case ObjectCategory.StaticPoint:
                 createdObj = Instantiate(_staticPointPrefab, entity.Position, Quaternion.identity);
+                createdObj.transform.SetParent(ParentCreatedObjects);
                 break;
             case ObjectCategory.Candy:
                 createdObj = Instantiate(_candyPrefab, entity.Position, Quaternion.identity);
+                createdObj.transform.SetParent(ParentCreatedObjects);
                 break;
             case ObjectCategory.Rope:
                 var obj = JObject.Parse(entity.ExpandProperty);
@@ -44,15 +48,18 @@ public class LevelSceneLoader : MonoBehaviour
                 int secondIndex = (int)obj["SecondNailIndex"];
 
                 createdObj = Instantiate(_ropePrefab, entity.Position, Quaternion.identity);
+                createdObj.transform.SetParent(ParentCreatedObjects);
                 Rope rope = createdObj.GetComponent<Rope>();
                 rope.RopeFirstObject = _listLoadedObj[firstIndex].transform;
                 rope.RopeSecondObject = _listLoadedObj[secondIndex].transform;
                 break;
             case ObjectCategory.Frog:
                 createdObj = Instantiate(_frogPrefab, entity.Position, Quaternion.identity);
+                createdObj.transform.SetParent(ParentCreatedObjects);
                 break;
             case ObjectCategory.Star:
                 createdObj = Instantiate(_starPrefab, entity.Position, Quaternion.identity);
+                createdObj.transform.SetParent(ParentCreatedObjects);
                 break;
             default:
                 Debug.LogError($"Unknown category: {entity.Category}");
