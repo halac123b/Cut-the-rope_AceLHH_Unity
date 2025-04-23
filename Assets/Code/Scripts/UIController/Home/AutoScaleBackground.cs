3c752f78ae0b7ab541b5240a856c76c9cd3afee5
Nguyen Trung Kien <Kie682003@gmail.com>
2025-04-23 21:43:23 +0700

    CTR-38 - Gắn UI màn hình Home đầu tiên

---
+++ Assets/Code/Scripts/UIController/Home/AutoScaleBackground.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using UnityEngine;
+
+public class AutoScaleBackground : MonoBehaviour
+{
+    [SerializeField] private Canvas _canvas;
+    public bool IsDelayAFrame;
+
+    public Canvas Canvas
+    {
+        get => _canvas;
+        set => _canvas = value;
+    }
+
+    private void Start()
+    {
+        if (IsDelayAFrame)
+        {
+            StartCoroutine(DelayedStartCoroutine());
+        }
+        else
+        {
+            UpdateBgSize();
+        }
+    }
+
+    private IEnumerator DelayedStartCoroutine()
+    {
+        yield return null;
+        UpdateBgSize();
+    }
+
+    private void UpdateBgSize()
+    {
+        if (_canvas == null)
+        {
+            _canvas = GetComponentInParent<Canvas>();
+        }
+
+        var canvasSize = _canvas.GetComponent<RectTransform>().sizeDelta;
+        var rect = GetComponent<RectTransform>();
+        var bgSize = rect.sizeDelta;
+
+        float ratio;
+
+        if (bgSize.x < canvasSize.x)
+        {
+            ratio = canvasSize.x / bgSize.x + 0.02f;
+        }
+        else if (bgSize.y < canvasSize.y)
+        {
+            ratio = canvasSize.y / bgSize.y + 0.02f;
+        }
+        else
+        {
+            return;
+        }
+
+        rect.sizeDelta = new Vector2(bgSize.x * ratio, bgSize.y * ratio);
+    }
+}
