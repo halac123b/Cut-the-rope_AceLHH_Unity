c778ab3822b8b49dd750700185861b3054838f54
Nguyen Trung Kien <Kie682003@gmail.com>
2025-04-30 14:27:50 +0700

    Merge main

--- Assets/Code/Scripts/UIController/Gameplay/PauseUI.cs
+++ Assets/Code/Scripts/UIController/Gameplay/PauseUI.cs
@@ -1,29 +1,17 @@
 using System;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PauseUI : MonoBehaviour
 {
-   [SerializeField] private Button _backBtn;
-   [SerializeField] private Button _resumeBtn;
-
    private void Start()
    {
-      _backBtn.onClick.AddListener(OnBackClick);
-      _resumeBtn.onClick.AddListener(OnResumeClick);
+      //gameObject.SetActive(false);
    }
 
    public void OnResumeClick()
    {
       Time.timeScale = 1;
       gameObject.SetActive(false);
-   }
-
-   public void OnBackClick()
-   {
-      StarController.SetStartLevel();
-      Time.timeScale = 1;
-      SceneManager.LoadScene("Home");
    }
 }
