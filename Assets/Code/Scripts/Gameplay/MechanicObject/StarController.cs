f44d9ccafd290bee7339a67e7d2469530dd1eff8
Nguyen Trung Kien <Kie682003@gmail.com>
2025-04-30 15:56:30 +0700

    CTR-45 - Làm UI Star đã collect trong scene GamePlay - [Refactor]

--- Assets/Code/Scripts/Gameplay/MechanicObject/StarController.cs
+++ Assets/Code/Scripts/Gameplay/MechanicObject/StarController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,17 +6,15 @@ public class StarController : MonoBehaviour
 {
     private static int _currentStarsInGameplay;
     private const int MAX_STARS = 3;
-    private static StarController Instance { get; set; }
     [SerializeField] private List<Animator> _stars;
 
-    private void Awake()
+    private void Start()
     {
-        Instance = this;
+        EventDispatcher.Instance.AddEvent(gameObject, _ => EnableStarIndex(_currentStarsInGameplay), EventDispatcher.OnStarIncreased);
     }
 
     public static int SetStartLevel()
     {
-        Debug.Log("[Kiên],[Star] Reset stars to 0");
         _currentStarsInGameplay = 0;
         return _currentStarsInGameplay;
     }
@@ -25,8 +24,7 @@ public class StarController : MonoBehaviour
         if (_currentStarsInGameplay < MAX_STARS)
         {
             _currentStarsInGameplay++;
-            Instance?.EnableStarIndex(_currentStarsInGameplay);
-            Debug.Log($"[Kien],[Star] Stars increased: {_currentStarsInGameplay}");
+            EventDispatcher.Instance.Dispatch(_currentStarsInGameplay, EventDispatcher.OnStarIncreased);
         }
 
         EventDispatcher.Instance.Dispatch(_currentStarsInGameplay, EventDispatcher.UpdateStarNumber);
@@ -44,9 +42,8 @@ public class StarController : MonoBehaviour
         if (starIndexInList >= 0 && starIndexInList < _stars.Count)
         {
             Animator star = _stars[starIndexInList];
-            star.speed = 0.8f; 
+            star.speed = 0.8f;
             star.SetTrigger("StarIncrease");
         }
     }
-
 }
