0beeb8da2fc5f960e0dcfc926ec17f64710b8a13
ntngan <ntngan020712@gmail.com>
2025-08-10 15:02:44 +0700

    Fixed: Logic Balloon

--- Assets/Code/Scripts/Gameplay/MechanicObject/Balloon.cs
+++ Assets/Code/Scripts/Gameplay/MechanicObject/Balloon.cs
@@ -1,27 +1,43 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Balloon : MonoBehaviour
 {
     public Camera mainCamera;
     
-    private float _balloonSpeed = 13f;
+    private float _balloonSpeed = 0.05f;
+    private bool _isCarryCandy;
 
     private void Start()
     {
         mainCamera = Camera.main;
+        _isCarryCandy = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Candy candy = collision.GetComponent<Candy>();
+        
         if (candy != null)
-        {
+        { 
+            candy.SetStaticGravity(); 
             transform.SetParent(candy.transform);
-            candy.SetBalloonState(true, _balloonSpeed );
+            _isCarryCandy = true;
         }
     }
 
-    public void PopBalloon()
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Candy candy = collision.GetComponent<Candy>();
+
+        if (candy != null)
+        { 
+            candy.SetBalloonState(true, _balloonSpeed);
+            transform.position = candy.transform.position;
+        }
+    }
+
+    private void PopBalloon()
     {
         Candy candy = transform.parent.GetComponent<Candy>();
         if (candy == null)
@@ -30,17 +46,24 @@ public class Balloon : MonoBehaviour
         }
         candy.SetBalloonState(false);
         Destroy(gameObject);
+        
+        _isCarryCandy = false;
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (!_isCarryCandy)
         {
-            Vector2 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            return;
+        }
+        
+        if (Pointer.current != null && Pointer.current.press.wasPressedThisFrame)
+        {
+            Vector2 mouseWorldPos = mainCamera.ScreenToWorldPoint(Pointer.current.position.ReadValue());
 
             RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero);
 
-            if (hit.collider != null && hit.collider.gameObject.CompareTag("Balloon"))
+            if (hit.collider != null && hit.collider.gameObject.CompareTag("Balloon") || hit.collider.gameObject.CompareTag("Candy"))
             {
                 PopBalloon();
             }
